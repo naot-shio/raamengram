4058a1c67e7bcec79ba197a5e6a48c531402bde7
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-20 16:31:31 +0900

    Created index page where you can create a new post and see the lits of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to root_url, notice: "#{@post.name} was successfully created"
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :price, :content)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  root 'posts#index'
+  post '/', to: 'posts#create'
   resources :posts
 end
