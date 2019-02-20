97bc9ed02bdc31f1bec05ee38f3a0208a9512d92
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-20 15:15:08 +0900

    Created navbar and landing page

--- db/migrate/20190220054712_create_posts.rb
+++ db/migrate/20190220054712_create_posts.rb
@@ -4,7 +4,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
       t.string :name
       t.text :content
       t.string :image
-      t.int :price
+      t.integer :price
 
       t.timestamps
     end
