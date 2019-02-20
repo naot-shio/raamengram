0b3c624e4c4ae2652fd91bae7f6b0acb04a3289f
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-20 22:46:45 +0900

    Installed minimagick and made it available to resize photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   has_one_attached :image
+
+  # def thumbnail
+  #   return self.image.variant(combine_options: {gravity: 'Center', crop: '180x135+0+0'}) %>
+  # end
 end
