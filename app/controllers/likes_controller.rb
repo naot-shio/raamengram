4afa5fd45eaacdc24a392896d0090f796c8d9a91
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-22 12:13:22 +0900

    Added like unlike functionality

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    post = Post.find(params[:post_id])
+    current_user.like(post)
+    redirect_to post
+  end
+
+  def destroy
+    post = Post.find(params[:post_id])
+    current_user.unlike(post)
+    redirect_to post
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'posts#index'
   post '/', to: 'posts#create'
-  resources :posts
-  resources :users, only: :show
+  resources :posts do
+    member do
+      resources :likes
+    end
+  end
+
+  resources :users, only: :show do
+  end
 end
