b3c014fdb23a4bf2ec4356cdf5d8b90859a627be
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-25 13:32:39 +0900

    Added area to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_043036) do
+ActiveRecord::Schema.define(version: 2019_02_25_043206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_043036) do
     t.string "broth"
     t.bigint "shop_id"
     t.string "shop"
+    t.string "area"
     t.index ["shop_id"], name: "index_posts_on_shop_id"
   end
 
