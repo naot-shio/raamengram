24032f0523bcc628c6f480588023c652a16e317d
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-26 12:39:44 +0900

    Added a test case for post's detail

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe 'Post', type: :system do
   describe 'Show the list of posts' do
-    let(:user_a) { FactoryBot.create(:user, name: 'UserA', username: 'UsernameA', email: 'usera@example.com' )}
-    let(:user_b) { FactoryBot.create(:user, name: 'UserB', username: 'UsernameB', email: 'userb@example.com' )}
+    let(:user_a) { FactoryBot.create(:user, name: 'UserA', username: 'UsernameA', email: 'usera@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'UserB', username: 'UsernameB', email: 'userb@example.com') }
+    let(:post_a) { FactoryBot.create(:post, name: 'Shop1', user: user_a)}
     
     before do
       FactoryBot.create(:post, name: 'Shop1', user:user_a)
@@ -11,20 +12,43 @@ describe 'Post', type: :system do
       fill_in 'Email', with: login_user.email
       fill_in 'Password', with: login_user.password
       click_button 'Log in'
-      visit user_path(login_user)
     end
 
-    context 'When user_a is logged in' do
-      let(:login_user) { user_a }
-      it 'should show the posts created by user1' do
-        expect(page).to have_content 'Shop1'
+    describe 'Show the list of posts that are created by individual users' do
+      context 'When user_a is logged in' do
+        let(:login_user) { user_a }
+        
+        before do
+          visit user_path(login_user)
+        end
+
+        it 'should show the posts created by user1' do
+          expect(page).to have_content 'Shop1'
+        end
+      end
+
+      context 'When user_b is logged in' do
+        let(:login_user) { user_b }
+        
+        before do
+          visit user_path(login_user)
+        end
+
+        it 'should not show the posts created by user1' do
+          expect(page).to have_no_content 'Shop1'
+        end
       end
     end
 
-    context 'When user_b is logged in' do
-      let(:login_user) { user_b }
-      it 'should not show the posts created by user1' do
-        expect(page).to have_no_content 'Shop1'
+    describe 'Show a post detail' do
+      let(:login_user) { user_a }
+
+      before do
+        visit  post_path(post_a)
+      end
+
+      it 'should show the detail for post a' do
+        expect(page).to have_content 'Shop1'
       end
     end
   end
