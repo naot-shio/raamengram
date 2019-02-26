247d80e21fddf3320adfd1cf4ecf665fad5a7a17
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-02-26 11:31:12 +0900

    Jotted down the first test case

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :post do
-    shop {'shop1'}
+    shop {'shop'}
     area {'tokyo'}
     name {'tonkotsu'}
     price {900}
     broth {'pork'}
-    description {'Jotting down a test case'}
+    content {'Jotting down a test case'}
     user
   end
 end
--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -6,16 +6,20 @@ describe 'Post', type: :system do
       # Create user_a
       user_a = FactoryBot.create(:user, name: 'UserA', username: 'UsernameA', email: 'usera@example.com')
       # User1 should be the tester
-      FactoryBot.create(:post, name: 'First post', user: user_a)
+      FactoryBot.create(:post, name: 'Shop1', user: user_a)
     end
     context 'When user_a is logged in' do
       before do
         # the tester should be logged in as user_a
+        visit new_user_session_path
+        fill_in 'Email', with: 'usera@example.com'
+        fill_in 'Password', with: 'password'
+        click_button 'Log in'
       end
 
       it 'should show the posts created by user1' do
         #show the list of posts
-
+        expect(page).to have_content 'Shop1'
       end
     end
   end
