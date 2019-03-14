132b3d064448c083303dbe1a061fe5ab64c8705e
Naoto Shiota <naotoshiota@NaotonoMacBook-Pro.local>
2019-03-14 12:14:56 +0900

    Added a user factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:name) { |n| "example#{n}"}
+    sequence(:email) { |n| "example#{n}@example.com"}
+    password 'password'
+    password_confirmation 'password'
+  end
+end
